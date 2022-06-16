931aacb3d8d2e28beec8c877d5df613931506aa4
5JJ <nedin12@naver.com>
2022-06-17 01:42:06 +0900

    Feat: add a function that gets all indexes of elements that get true from callback function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,4 +3,17 @@ type loopType<T> = (times: number, callback: (index: number) => T) => T[];
 export const loop: loopType<any> = (times, callback) =>
   new Array(times).fill(0).map((_, index) => callback.call({}, index));
 
+type findAllIndexesType = <T>(
+  arr: T[],
+  callback: (item: T, index?: number) => boolean
+) => number[];
+
+export const findAllIndexes: findAllIndexesType = (arr, callback) =>
+  arr.reduce((acc, item, index) => {
+    if (callback.call({}, item, index)) {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+
 export default {};
