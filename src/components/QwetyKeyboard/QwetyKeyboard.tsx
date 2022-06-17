59af7e4d474872f93c6a193a88754cd65fa9b818
5JJ <nedin12@naver.com>
2022-06-17 22:27:43 +0900

    Feat: add qwety keyboard component

---
+++ src/components/QwetyKeyboard/QwetyKeyboard.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+
+import { KEY_BACKSPACE, KEY_ENTER } from "constants/common";
+
+import QwetyKeyboardType, {
+  onClickFnType,
+  keycapType,
+} from "./QwetyKeyboard.types";
+
+const QWETY_ALPHABETS = [
+  ["ㅂ", "ㅈ", "ㄷ", "ㄱ", "ㅅ", "ㅛ", "ㅕ", "ㅑ"],
+  ["ㅁ", "ㄴ", "ㅇ", "ㄹ", "ㅎ", "ㅗ", "ㅓ", "ㅏ", "ㅣ"],
+  ["ㅋ", "ㅌ", "ㅊ", "ㅍ", "ㅠ", "ㅜ", "ㅡ"],
+];
+
+const Keycap = styled.button(({}) => ({
+  margin: "1px",
+  display: "inline-block",
+  width: "30px",
+  height: "32px",
+  verticalAlign: "middle",
+  textAlign: "center",
+  lineHeight: "32px",
+  fontSize: "13px",
+  fontWeight: "600",
+  cursor: "pointer",
+  backgroundColor: "lightgrey",
+}));
+
+const backgroundCSS = {
+  backgroundSize: "60%",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+};
+
+const BackSpaceKeycap = styled(Keycap)(({}) => ({
+  backgroundImage: 'url("./icons/backspace.png")',
+  ...backgroundCSS,
+}));
+
+const EnterKeycap = styled(Keycap)(({}) => ({
+  backgroundImage: 'url("./icons/enter.png")',
+  ...backgroundCSS,
+}));
+
+const KeyboardContainer = styled.div(({}) => ({
+  textAlign: "center",
+}));
+
+function QwetyKeyboard(props: QwetyKeyboardType) {
+  const { clickCallback } = props;
+
+  const handleClick: onClickFnType = (value) => {
+    clickCallback(value);
+  };
+
+  return (
+    <KeyboardContainer>
+      {QWETY_ALPHABETS.map((list, index) => (
+        <div key={index}>
+          {index === 2 && (
+            <BackSpaceKeycap
+              type="button"
+              tabIndex={0}
+              onClick={() => handleClick(KEY_BACKSPACE)}
+            ></BackSpaceKeycap>
+          )}
+          {list.map((alphabet: keycapType) => (
+            <Keycap
+              type="button"
+              tabIndex={0}
+              key={alphabet}
+              onClick={() => handleClick(alphabet)}
+            >
+              {alphabet}
+            </Keycap>
+          ))}
+          {index === 2 && (
+            <EnterKeycap
+              type="button"
+              tabIndex={0}
+              onClick={() => handleClick(KEY_ENTER)}
+            ></EnterKeycap>
+          )}
+        </div>
+      ))}
+    </KeyboardContainer>
+  );
+}
+
+export default QwetyKeyboard;
---
+++ src/components/QwetyKeyboard/QwetyKeyboard.types.ts
@@ -0,0 +1,19 @@
+import {
+  KR_CONSONANTS,
+  KR_VOWELS,
+  KEY_ENTER,
+  KEY_BACKSPACE,
+} from "constants/common";
+
+interface QwetyKeyboardType {
+  clickCallback: (value: keycapType) => void;
+}
+
+export type onClickFnType = (value: keycapType) => void;
+export type keycapType =
+  | typeof KR_CONSONANTS[number]
+  | typeof KR_VOWELS[number]
+  | typeof KEY_ENTER
+  | typeof KEY_BACKSPACE;
+
+export default QwetyKeyboardType;
