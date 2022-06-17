a2f689080efe3a2807cbee40fb78c048f50f07cb
5JJ <nedin12@naver.com>
2022-06-17 22:14:11 +0900

    Feat: add common constants - game menu list - keycap

---
+++ src/constants/common.ts
@@ -0,0 +1,47 @@
+export const GAME_KORDLE = "kordle";
+export const GAME_HANGMAN = "hangman";
+
+export const GAME_LIST_IN_KR = {
+  [GAME_KORDLE]: "꼬들",
+  [GAME_HANGMAN]: "행맨",
+};
+
+export const KEY_ENTER = "enter";
+export const KEY_BACKSPACE = "backspace";
+
+export const BLOCK_RESULT = {
+  CORRECT: "c",
+  MISMATCH: "m",
+  FAILED: "f",
+} as const;
+
+export const KR_CONSONANTS = [
+  "ㄱ",
+  "ㄴ",
+  "ㄷ",
+  "ㄹ",
+  "ㅁ",
+  "ㅂ",
+  "ㅅ",
+  "ㅇ",
+  "ㅈ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+] as const;
+export const KR_VOWELS = [
+  "ㅏ",
+  "ㅑ",
+  "ㅓ",
+  "ㅕ",
+  "ㅗ",
+  "ㅛ",
+  "ㅜ",
+  "ㅠ",
+  "ㅡ",
+  "ㅣ",
+] as const;
+
+export default {};
