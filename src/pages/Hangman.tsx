7fb02c2cf076a6068322adf0c5c3389815cd998a
5JJ <nedin12@naver.com>
2022-07-16 18:15:47 +0900

    Feat: set Layout of the Hangman page

--- src/pages/Hangman.tsx
+++ src/pages/Hangman.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useMemo } from "react";
+import Menu from "components/Menu";
+
+import { GAME_LIST_IN_KR, GAME_HANGMAN } from "constants/common";
 
 export default function HangMan() {
-  return <h1>HangMan</h1>;
+  const gameList = useMemo(
+    () =>
+      Object.keys(GAME_LIST_IN_KR).map((gameName) => ({
+        name: GAME_LIST_IN_KR[gameName],
+        link: gameName,
+      })),
+    []
+  );
+
+  return (
+    <>
+      <Menu selectedItem={GAME_HANGMAN} menuList={gameList} />
+    </>
+  );
 }
